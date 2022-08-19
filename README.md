e255455f485537c945d6c1132956cfd2a40fb6d0
meeramadhavi <106495202+meeramadhavi@users.noreply.github.com>
2022-08-19 13:07:21 -0700

    This is new version with an added python file

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # demo
 This is a practice repository
 this is an update
+Sometimes all i think about is food
---
+++ test.py
@@ -0,0 +1,9 @@
+# %%
+print(10 + 12)
+
+
+# %%
+
+print(365*7)
+# %%
+#you can make cells in this by using '# %%' to make sections!
