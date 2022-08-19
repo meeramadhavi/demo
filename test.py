cb4fb6ad8dbea87c885608c7e66cf02410b32f5a
Meera <fmeera@ucdavis.edu>
2022-08-19 13:51:22 -0700

    update

--- test.py
+++ test.py
@@ -7,5 +7,7 @@ print(10 + 12)
 print(365*7)
 # %%
 #you can make cells in this by using '# %%' to make sections!
-
+#meomweowejowiehoehfoiernf
 # I have made changes to this python file
+print("blue")
+# %%
